ed6159e5590cfe649fb46972277f3a47ef81048a
Naysayer <n77ox@yandex.ru>
2023-06-28 23:25:09 +0300

    Validation error message

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let addButton = document.querySelector('.add-btn');
-let textInput = document.querySelector('.text-input');
-let list = document.querySelector('.list');
+const form = document.querySelector(".needs-validation")
+const textInput = document.querySelector(".text-input");
+const list = document.querySelector(".list");
 
-addButton.onclick = function() {
-    if(textInput.value)
-    {
-        let newListElement = document.createElement("li");
-        let content = document.createTextNode(textInput.value);
-        newListElement.appendChild(content);
-        list.appendChild(newListElement);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-        textInput.value = "";
-    }
-}
+  const inputValue = textInput.value.trim();
+  textInput.value = "";
+
+  if (!inputValue) {
+    form.classList.add("was-validated");
+    return 0;
+  }
+
+  const newListElement = document.createElement("li");
+  const content = document.createTextNode(inputValue);
+  newListElement.appendChild(content);
+  list.appendChild(newListElement);
+
+  form.classList.remove("was-validated");
+});
